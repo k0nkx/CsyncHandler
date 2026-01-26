0ac19469cef5aa7d1a55cdccd550e2e855436ad9
k0nkx <140971614+k0nkx@users.noreply.github.com>
2026-01-25 16:37:39 -0800

    Update Handler.lua

--- Handler.lua
+++ Handler.lua
@@ -6,7 +6,7 @@ local CsyncHandler = {
     PositionHistory = {},
     LockedPosition = nil,
     LockedUntil = nil,
-    ToggleKey = Enum.KeyCode.V,
+    ToggleKey = Enum.KeyCode.Keypad8,
     
     Config = {
         Position = {
